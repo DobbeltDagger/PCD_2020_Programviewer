9ef3919c69dc5ee894e718874625e7d30ac20986
Anders Visti <andersvisti@gmail.com>
2020-02-18 10:41:25 +0100

    Added first image change test to wrapper

--- js/App.js
+++ js/App.js
@@ -1,5 +1,6 @@
 function App() {
   
+  this.currImg = '';
   
 }
 
@@ -10,6 +11,17 @@ App.prototype = {
   ////////////////////////////////
   init: function() {
     console.log("App is initiated");
+    this.renderImage('hep')
+
   },
+
+
+  renderImage: function(imgPath) {
+
+    let test = 'url(images/Program.jpg)';
+
+    // find elm
+    let elm = document.getElementById('imgWrapper').style.backgroundImage = test;
+  }
 
 }
