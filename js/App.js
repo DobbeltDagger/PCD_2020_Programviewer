44b4148df61f844cb64723f300c48d8432670b3c
Anders Visti <andersvisti@gmail.com>
2020-02-18 16:09:15 +0100

    Added clock and some more time functionality

--- js/App.js
+++ js/App.js
@@ -32,14 +32,30 @@ App.prototype = {
   // get the time - hours and minutes
   checkTime: function() {
 
-    let now = this.getNow();
+    const now = this.getNow();
     console.log("now:", now);
 
-
-    // go through time here
-    for(let i=0; i < this.schedule.length; i++) {
+    // go through time here - backwards
+    for(let i = this.schedule.length-1; i >= 0; i--) {
       console.log("sched[i]", this.schedule[i]);
+
+      // shorten the time vars
+      const scHours = this.schedule[i].hours;
+      const scMins = this.schedule[i].mins;
+
+      // compare now to schedule times
+      if (now.hours >= scHours && now.mins >= scMins) {
+        this.currImg = this.schedule[i].imgUrl;
+      }
+
     }
+
+    // put time in interface
+    let showHours = ''; let showMins = ''; 
+    (now.hours < 10) ? showHours = '0'+now.hours : showHours = now.hours;
+    (now.mins < 10) ? showMins = "0"+now.mins : showMins = now.mins;
+
+    document.getElementById('txtWrapper').innerHTML = showHours + ":" + showMins;
 
   },
 
@@ -68,6 +84,7 @@ App.prototype = {
   renderImage: function(imgPath) {
 
     let test = 'url(images/Program.jpg)';
+    // test = 'url(images/' + imgPath + ')';
 
     // find elm
     let elm = document.getElementById('imgWrapper').style.backgroundImage = test;
@@ -83,77 +100,77 @@ App.prototype = {
       {
         "hours": 10,
         "mins": 15,
-        "imgUrl": "",
+        "imgUrl": "slides2.jpg",
         "desc": "Workshop #1: Live Coding with Sonic Pi  Frederik la Cour & Rolf Holm"
       },
       // BREAK
       {
         "hours": 11,
         "mins": 30,
-        "imgUrl": "",
+        "imgUrl": "slides3.jpg",
         "desc": "BREAK"
       },
       // Introduction
       {
         "hours": 11,
         "mins": 45,
-        "imgUrl": "",
+        "imgUrl": "slides4.jpg",
         "desc": "Introduction by PCD organizers"
       },
       // Open and Close - Gabriel
       {
         "hours": 11,
         "mins": 55,
-        "imgUrl": "",
+        "imgUrl": "slides5.jpg",
         "desc": "Open and Close — Creating Other Archives  Gabriel Pereira"
       },
       // Juicy Blockchain - Anna
       {
         "hours": 12,
         "mins": 15,
-        "imgUrl": "",
+        "imgUrl": "slides6.jpg",
         "desc": "A Juicy Blockchain Story about Governance  Anna Brynskov"
       },
       // LUNCH
       {
         "hours": 12,
         "mins": 30,
-        "imgUrl": "",
+        "imgUrl": "slides7.jpg",
         "desc": "LUNCH"
       },
       // Sonic Surveys - Alex
       {
         "hours": 13,
         "mins": 00,
-        "imgUrl": "",
+        "imgUrl": "slides8.jpg",
         "desc": "Sonic Surveys and Disruptions of Denmark  Alex Mørch"
       },
       // Utopia + - Mogens
       {
         "hours": 13,
         "mins": 20,
-        "imgUrl": "",
+        "imgUrl": "slides9.jpg",
         "desc": "Utopia +  Mogens Jacobsen"
       },
       // Fermenting Data -Magda
       {
         "hours": 13,
         "mins": 40,
-        "imgUrl": "",
+        "imgUrl": "slides10.jpg",
         "desc": "Fermenting Data: Sampling and Other Experiments with Microbes, Plants, People and Culture Machines  Magda Tyżlik-Carver"
       },
       // BREAK
       {
         "hours": 14,
         "mins": 05,
-        "imgUrl": "",
+        "imgUrl": "slides11.jpg",
         "desc": "BREAK"
       },
       // Workshop #2 - Lasse og Niels
       {
         "hours": 14,
         "mins": 15,
-        "imgUrl": "",
+        "imgUrl": "slides12.jpg",
         "desc": "Workshop #2: Machine Learning with P5.js  Lasse Korsgaard & Niels Konrad"
       },
       // Social time!
